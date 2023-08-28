e48d65f3e0ef236ff90040054ad10a398b35abf3
KrivorotovNV <nicamx@yandex.ru>
2023-08-28 16:47:56 +0300

    работает частично

--- src/test/java/guru/qa/junit_work/pages/ParameterizedPage.java
+++ src/test/java/guru/qa/junit_work/pages/ParameterizedPage.java
@@ -2,16 +2,22 @@ package guru.qa.junit_work.pages;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.text;
+import java.util.List;
+
+
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+
 
 public class ParameterizedPage {
 
    // ParameterizedPage parameterizedPage=new ParameterizedPage();
     SelenideElement
-        selectRu=$("#langs");
+        selectRu=$("#langs"),
+        menu =$("#menu");
+
 
     public ParameterizedPage openPage() {
         open("https://paraavis.com/");
@@ -20,6 +26,17 @@ public class ParameterizedPage {
     public  ParameterizedPage setLocale(Locale locale) {
         //selectRu.$(byText(locale.name())).click();
        selectRu.find(byText(locale.name())).click();
+        return this;
+    }
+
+    public  ParameterizedPage setLocale1(String locale) {
+        //selectRu.$(byText(locale.name())).click();
+        selectRu.find(byText(locale)).click();
+        return this;
+    }
+    public  ParameterizedPage shouldHave(List<String> list) {
+
+        //menu.shouldHave(texts(list));
 
         return this;
     }
